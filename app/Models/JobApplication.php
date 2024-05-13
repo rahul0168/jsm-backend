0e30e38187ef23dc8aee543342dee613abe4a28b
devteam0111 <166615747+devteam0111@users.noreply.github.com>
2024-05-13 17:36:44 +0530

    first commit

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class JobApplication extends Model
 {
     use HasFactory;
+
+    protected $table = 'job_applications';
+
+
+    protected $fillable = [
+        'job_id',
+        'user_id',
+    ];
 }
